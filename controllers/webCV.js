d45b1ab4a6bc5465fa10626d6784ae75574635da
Maxine Ellah <maxine.ellah@gmail.com>
2016-07-13 16:50:04 +1200

    removed link to google drive

--- controllers/webCV.js
+++ controllers/webCV.js
@@ -15,7 +15,7 @@ app.controller('webCV', function($scope) {
   },
   {
     text: 'Resume',
-    link: 'https://docs.google.com/document/d/1lnLSrh6rwcikrYyQd7dF8p_pRk9t9CfQ36cHBDt6fSA/pub'
+    link: '/images/MaxineEllahCV.pdf'
   },
   {
     text: 'Contact',
