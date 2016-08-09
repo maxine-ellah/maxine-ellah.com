e5e3415e42a85a45bdcdd833f534ac51872b9a21
Maxine Ellah <maxine.ellah@gmail.com>
2016-08-09 12:42:31 +1200

    change cv name

--- controllers/webCV.js
+++ controllers/webCV.js
@@ -15,7 +15,7 @@ app.controller('webCV', function($scope) {
   },
   {
     text: 'Resume',
-    link: '/images/MaxineEllahCVdnowrng.pdf'
+    // link: '/images/MaxineEllahCV.pdf'
   },
   {
     text: 'Contact',
